a24d2cd319d217b0dd0f031ac69021d1412a34b5
Jisoo Lee (Jisoo Kim) <98534676+Panton99@users.noreply.github.com>
2022-12-06 13:33:37 -0500

    Update style.js

--- style.js
+++ style.js
@@ -1,12 +1,13 @@
+//Dark mode button
 let button = document.getElementById("dark-button");
 button.addEventListener("click", onClick());
-
 
 function onClick() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
 
+//Form submission
 const valSubmit = async (event) => {
     event.preventDefault();
     const form = event.target;
@@ -22,41 +23,3 @@ const valSubmit = async (event) => {
 };
 document.querySelector("form").addEventListener("submit", valSubmit);
 
-
-
-
-// function validateName(val, status) {
-//     var regex = /^[a-zA-Z]*$/;
-//     if(val.value.length > 0) {
-//         return true;
-//     }
-//     if (regex.test(val.value)) {
-//         return true;
-//     }
-// }
-
-// function validiation(val) {
-//     val.preventDefault();
-//     var data = new FormData(val.target);
-//     valFName = validateName(document.getElementById('firstName'), valFName);
-//     valLName = validateName(document.getElementById('lastName'), valLName);
-//     var msg = documnte.getElementById("submitMsg");
-
-//     if (!valFName || !valLName) {
-//         alert("Type your name.");
-//     }
-
-//     fetch(val.target.action, {
-//         method: form.method,
-//         body: data,
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     }).then(res => {
-//         msg.innerHTML = "Thank you for the submission!";
-//         form.reset();
-//     }).catch(error => {
-//         msg.innerHTML = "There was a problem submitting your form. Try again."
-//     });
-// }
-// form.addEventListener("submit", validiation)
